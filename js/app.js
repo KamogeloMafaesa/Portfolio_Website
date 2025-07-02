95164cd680cc918c0a60c264b29e2313465046b6
KamogeloMafaesa <53045373+KamogeloMafaesa@users.noreply.github.com>
2025-07-02 14:49:07 +0200

    turned nav bar into a component
    added reusable navigation bar code

--- js/app.js
+++ js/app.js
@@ -1,10 +1,39 @@
+//fetch navbar and inject it to every html page
+
+function GetNavBar(){
+fetch('../components/navbar.html')
+.then(response => {
+    return response.text()
+})
+.then(function(html){
+
+    //to convert the HTML string into a document
+    var parser = new DOMParser();
+    var navPage = parser.parseFromString(html, 'text/html');
+
+    var navigationBar = navPage.querySelector('nav');
+    //injecting navbar into page
+    document.getElementById('navibar').innerHTML = navigationBar.outerHTML;
+
+    //adding event listeners for hamburger menu when screen is smaller
 
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 const links = document.querySelectorAll('.nav-links li');
 
 
-hamburger.addEventListener("click", ()=>{
+hamburger.addEventListener('click', ()=>{
+    
+    navLinks.classList.toggle("open");
+    
+    links.forEach(link=>{
+        link.classList.toggle("fade");
+          
+    });
+});
+
+//for phones and devices with touch screens
+hamburger.addEventListener("touchstart", ()=>{
     
     navLinks.classList.toggle("open");
     
@@ -15,16 +44,18 @@ hamburger.addEventListener("click", ()=>{
 });
 
 
-
-hamburger.addEventListener("touch", ()=>{
-    
-    navLinks.classList.toggle("open");
-    
-    links.forEach(link=>{
-        link.classList.toggle("fade");
-          
-    });
+})
+.catch(function (error){
+    console.warn('Could not find your file mamasita', error)
 });
+ 
+}
+//adding event listener to call Get NavBar page on load
+window.addEventListener('DOMContentLoaded', GetNavBar);
+
+
+
+
 
 
 
@@ -32,7 +63,7 @@ hamburger.addEventListener("touch", ()=>{
 
 
 //typewriter
-const titles = ["An XR DEVELOPER." ,"A FRONT-END DEVELOPER.","A GAME DEVELOPER", "A GENERALIST"];
+const titles = ["AN XR DEVELOPER." ,"A FRONT-END DEVELOPER.","A GAME DEVELOPER", "A SOFTWARE DEVELOPER"];
 
 let i = 0;
 let counter;
